b7c2460d872d3f4366d9f5cc0fb3801ff9ec0615
Raul Tierno <raultm@gmail.com>
2026-06-27 09:42:58 +0200

    feat: añadir iconos para instalar como app y rediseñar controles del metrónomo
    - icono SVG propio por herramienta (diapasón, nota+check, metrónomo) con
      variantes PNG (180/192/512) para apple-touch-icon y manifest.json
    - manifest.json por herramienta (display standalone) más uno para la
      landing, para que cada una se instale con su propio icono en
      Android/iOS
    - favicon también en la landing page
    - los sw.js cachean ya los iconos/manifest y activan la versión nueva al
      instante (skipWaiting + clients.claim) en vez de esperar a cerrar todas
      las pestañas
    - metrónomo: BPM y pulsos por compás pasan de slider/input numérico a un
      stepper grande (-5/-1/+1/+5 y -1/+1) con pastillas de valores
      habituales (60/90/120 bpm, 2/3/4/6 pulsos), todo centrado

--- practica/sw.js
+++ practica/sw.js
@@ -1,21 +1,25 @@
-const CACHE_NAME = 'practica-v1';
+const CACHE_NAME = 'practica-v2';
 const ASSETS = [
   './',
   './index.html',
-  './pitch.js'
+  './pitch.js',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
